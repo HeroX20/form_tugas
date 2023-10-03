839832acfb042c6f0e4a7a72d7bb046c04662e28
HeroX20 <142101241+HeroX20@users.noreply.github.com>
2023-10-03 23:06:59 +0700

    Update Main.js

--- Main.js
+++ Main.js
@@ -2,8 +2,6 @@ const toggleBtn = document.getElementById('mode');
 const body = document.body;
 const header = document.querySelector('header');
 const daftarTugas = document.getElementById('daftarTugas');
-const openLinkButton = document.getElementById('openLinkButton');
-const love = "egg/love.html"; // Ganti dengan tautan yang sesuai
 const hapusSemuaButton = document.getElementById('hapusSemuaBtn');
 
 toggleBtn.addEventListener('click', () => {
@@ -18,7 +16,7 @@ document.getElementById('formTugas').addEventListener('submit', function(event) 
     let mataPelajaran = document.getElementById('mataPelajaran').value;
     let infoTugas = document.querySelector('input[name="infoTugas"]:checked').value;
     let tenggat = document.getElementById('tenggat').value;
-    checkAndShowButton(tenggat);
+    easteregg(tenggat);
     let keterangan = document.getElementById('keterangan').value.trim();
 
     if (mataPelajaran !== '' && tenggat !== '') {
@@ -60,7 +58,14 @@ function tambahTugasKeDaftar(mataPelajaran, infoTugas, tenggat, keterangan) {
     hapusButton.classList.add('hapus-btn');
     li.appendChild(hapusButton);
 
-    daftarTugas.appendChild(li);
+    const ulDaftarTugas = document.getElementById('daftarTugas');
+    const firstLi = ulDaftarTugas.firstChild; // Mendapatkan elemen li pertama
+
+    if (firstLi) {
+        ulDaftarTugas.insertBefore(li, firstLi); // Menyisipkan li baru sebelum li pertama
+    } else {
+        ulDaftarTugas.appendChild(li); // Jika daftar kosong, tambahkan li baru
+    }
 
     const tugas = {
         mataPelajaran,
@@ -85,7 +90,7 @@ function tambahTugasKeDaftar(mataPelajaran, infoTugas, tenggat, keterangan) {
 daftarTugas.addEventListener('click', function(event) {
     if (event.target.classList.contains('salin-btn')) {
         const liElement = event.target.closest('li');
-        const mataPelajaran = liElement.querySelector('.mata-pelajaran').textContent;
+        const mataPelajaran = getMapelSingkat(liElement.querySelector('.mata-pelajaran').textContent);
         const infoTugas = liElement.querySelector('.info-tugas').textContent;
         const tenggat = liElement.querySelector('.tenggat').textContent.replace('Tenggat: ', '');
         const keterangan = liElement.querySelector('.keterangan').textContent.replace('Keterangan: ', '');
@@ -114,8 +119,57 @@ function hapusTugas(id) {
     localStorage.setItem('daftarTugas', JSON.stringify(updatedData));
 }
 
+function hapusTugasTenggat() {
+    const ulDaftarTugas = document.getElementById('daftarTugas');
+    const lis = ulDaftarTugas.getElementsByTagName('li');
+    const tanggalHariIni = new Date();
+
+    for (let i = 0; i < lis.length; i++) {
+        const li = lis[i];
+        const tenggatText = li.querySelector('.tenggat').textContent;
+        const tenggatTanggal = new Date(formatTanggalFromString(tenggatText));
+
+        if (tenggatTanggal < tanggalHariIni) {
+            li.remove();
+        }
+    }
+}
+
+// Fungsi bantu untuk mengonversi tanggal dari string menjadi objek Date
+function formatTanggalFromString(tanggalString) {
+    const tanggalArray = tanggalString.replace('Tenggat: ', '').split('/');
+    const tahun = parseInt(tanggalArray[2]);
+    const bulan = parseInt(tanggalArray[1]) - 1; // Ingat, bulan dimulai dari 0 (Januari = 0)
+    const tanggal = parseInt(tanggalArray[0]);
+    return new Date(tahun, bulan, tanggal);
+}
+
+// Jalankan fungsi hapusTugasTenggat saat halaman dimuat
+document.addEventListener('DOMContentLoaded', function() {
+    hapusTugasTenggat();
+});
+
 function formatTugasText(mataPelajaran, infoTugas, tenggat, keterangan) {
-    return `#${mataPelajaran}\n#${infoTugas}\nTenggat: ${tenggat}\nKeterangan: ${keterangan}`;
+    const mapelSingkat = getMapelSingkat(mataPelajaran);
+    return `#${mapelSingkat}\n#${infoTugas}\nTenggat: ${tenggat}\nKeterangan: ${keterangan}`;
+}
+
+function getMapelSingkat(mataPelajaran) {
+    const mapelSingkat = {
+        "Pendidikan Agama Islam": "PAI",
+        "PRODUKTIF": "PRODUKTIF",
+        "Bahasa Inggris": "B.Ing",
+        "Bahasa Indonesia": "B.Indo",
+        "Pendidikan Jasmani, Olahraga dan Kesehatan": "PJOK",
+        "Matematika": "MTK",
+        "Pendidikan Kewarganegaraan": "PKN",
+        "Sejarah": "Sejarah",
+        "Produk Kreatif dan Kewirausahaan": "PKWU",
+        "Konsentrasi Keahlian": "KK",
+        "Lainnya":"Lainnya"
+    };
+
+    return mapelSingkat[mataPelajaran] || mataPelajaran;
 }
 
 function salinTeks(tekstosalin) {
@@ -141,24 +195,26 @@ function formatTanggal(tanggal) {
     return tanggalFormatted;
 }
 
-function checkAndShowButton(tenggat) {
+function easteregg(tenggat, keterangan) {
     const targetTanggal = new Date('2006-05-20');
     const inputTanggal = new Date(tenggat);
+    
+    const keteranganTextarea = document.getElementById('keterangan');
+    const kataKunci = 'kalkulator bunga'; //kata rahasia atau kata kunci.
+    // const kataKunci1 = 'data'; //kata rahasia atau kata kunci.
 
     if (inputTanggal.getTime() === targetTanggal.getTime()) {
-        openLinkButton.style.display = 'block';
-    } else {
-        openLinkButton.style.display = 'none';
+        window.open('egg/love.html', '_blank');
+    } else if (keteranganTextarea.value.toLowerCase().includes(kataKunci)) {
+        window.open('kalkulator/index.html', '_blank');
     }
+    // else if (keteranganTextarea.value.toLowerCase().includes(kataKunci1)) {
+    //     window.open('kalkulator/style.css', '_blank');
+    // }
 }
 
-openLinkButton.addEventListener('click', function() {
-    window.open(love, '_blank');
-});
 
-
-
-var update = `update :\n1.local storage\n2.hapus button\n3.hapus semua button\n4.bug fixed\n`;
-var info = `indev: \n1.membuat tugas yang bisa upload dan download/bisa di lihat orang lain\n2.membuat tampilan pilihan antara kelas X,XI,XII \n3.membuat login page agar pihak tertentu dapet mengedit`;
+var update = `update :\n1. bug fixed\n2.fitur hapus tugas jika sudah melewati tenggat(beta)\n3. mapel dipersingkat`;
+var info = `indev: \n1. tombol menu\n2.fitur lainnya`;
 console.log(update);
 console.log(info);
